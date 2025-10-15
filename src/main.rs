d51bc45ec367573812c08cb6fe587b792c93108b
Malte <Maltebraun@outlook.com>
2025-10-15 16:51:11 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn taupunkt(temperatur: f64, luftfeuchtigkeit: f64) -> f64 {
+    let (a, b) = if temperatur >= 0.0 {
+        (17.62, 243.12) // wenn die Temperatur >= 0°C ist
+    } else {
+        (22.46, 272.62) // wenn die Temperatur <= 0°C ist
+    };
+
+    let rh = luftfeuchtigkeit / 100.0;
+    let alpha = rh.ln() + (a * temperatur) / (b + temperatur);
+    b * alpha / (a - alpha)
+}
+
+fn main() {
+    let mut input = String::new();
+
+    println!("Bitte Temperatur in °C eingeben:");
+    io::stdin().read_line(&mut input).expect("Fehler beim Lesen"); // liest die Eingabe des Benutzers als Text ein
+    let temperatur: f64 = input.trim().parse().expect("Ungültige Zahl"); //wandelt die Eingabe in eine Gleitkommazahl (f64)
+    input.clear(); //leert den Eingabepuffer
+
+    println!("Bitte relative Luftfeuchtigkeit in % eingeben:");
+    io::stdin().read_line(&mut input).expect("Fehler beim Lesen");
+    let luftfeuchtigkeit: f64 = input.trim().parse().expect("Ungültige Zahl");
+
+    let taupunkt = taupunkt(temperatur,luftfeuchtigkeit);
+
+    println!("Bei {}°C und einer luftfeuchtigkeit von {}% liegt der Taupunkt bei {:.2}°C", temperatur, luftfeuchtigkeit, taupunkt)
+}
